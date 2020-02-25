aa359f21a03abc9ca4b458aec013de7601cb58f0
nature-god <2726916340@qq.com>
2020-02-25 18:00:17 +0800

    Add Upload Method

--- UnityProgram/FTPFileConnect/Assets/Scripts/GetFileFromFTP.cs
+++ UnityProgram/FTPFileConnect/Assets/Scripts/GetFileFromFTP.cs
@@ -7,34 +7,45 @@ using System.Net;
 using System.Text;
 using UnityEngine.UI;
 
-public class GetFileFromFTP : MonoBehaviour {
-
-    private const int ftpport = 21;                 //端口号
+public class GetFileFromFTP : MonoBehaviour
+{
+    private const int ftpport = 14149;              //端口号
     private string ftpUrlstring = null;             //FTP文件路径
     private NetworkCredential networkCredential;    //网络证书
     private string ftppath = "127.0.0.1:14149";     //FTP服务器地址
     private string currentDir = "/";                //用于记录当前层级目录
 
-    public string downname;
+    private string FTPLoginUser;                     //FTP服务器登陆用户名
+    private string FTPLoginPassword;                 //FTP服务器用户登陆密码
 
     public Transform UIPanel;                       //FTP服务器展示页面
     public GameObject DirPrefab;                    //文件夹Prefab
     public GameObject FilePrefab;                   //文件Prefab
     public GameObject GetBackButton;                //返回上级按钮
+    public Slider slider;                           //文件下载进度条
 
-    private int anchorIndex = 0;
-    private Vector2 anchorPos = new Vector2(-160.0f,0.0f);
+    private int anchorIndex = 0;                    //用于遍历显示文件排序
+    private Vector2 anchorPos = new Vector2(-160.0f, 0.0f);  //默认排序初始位置
     private Vector2 AnchorPos
     {
         get
-        {           
+        {
             anchorPos.x = -160.0f + (80.0f * (anchorIndex % 5));
             anchorPos.y = 0.0f - (60.0f * (anchorIndex / 5));
             anchorIndex++;
             return anchorPos;
         }
-    }
+    }                    //迭代器自动迭代排序位置
 
+    /// <summary>
+    /// 初始状态设置，关闭下载进度条
+    /// </summary>
+    private void Start()
+    {
+        slider.value = 0.0f;
+        slider.GetComponentInChildren<Text>().text = "0KB";
+        slider.gameObject.SetActive(false);
+    }
 
     /// <summary>
     /// 创建FTP服务器连接
@@ -42,7 +53,7 @@ public class GetFileFromFTP : MonoBehaviour {
     /// <param name="url">FTP服务器地址</param>
     /// <param name="requestMethod">FTP服务器请求方法</param>
     /// <returns></returns>
-    public FtpWebRequest CreatFtpWebRequest(string url,string requestMethod)
+    private FtpWebRequest CreatFtpWebRequest(string url, string requestMethod)
     {
         FtpWebRequest request = (FtpWebRequest)FtpWebRequest.Create(url);
         request.Credentials = networkCredential;
@@ -57,7 +68,7 @@ public class GetFileFromFTP : MonoBehaviour {
     /// </summary>
     /// <param name="request">发出的对FTP服务器请求</param>
     /// <returns></returns>
-    public FtpWebResponse GetFtpResponse(FtpWebRequest request)
+    private FtpWebResponse GetFtpResponse(FtpWebRequest request)
     {
         FtpWebResponse response = null;
         try
@@ -65,30 +76,45 @@ public class GetFileFromFTP : MonoBehaviour {
             response = (FtpWebResponse)request.GetResponse();
             return response;
         }
-        catch(WebException ex)
+        catch (WebException ex)
         {
             Debug.LogError(ex.Message);
             return null;
         }
     }
 
+    /// <summary>
+    /// 点击连接FTP服务器Button的响应事件
+    /// </summary>
     public void OnClickLoginButton()
+    {
+        ConnectToFTPServer();
+    }
+
+    /// <summary>
+    /// 连接FTP服务器，并在console界面展示连接返回结果
+    /// </summary>
+    public void ConnectToFTPServer()
     {
         ftpUrlstring = "ftp://" + ftppath;
         networkCredential = new NetworkCredential("nature", "Ttrby666");
-        if(ShowFtpFileAndDirectory() == true)
+        if (ShowFtpFileAndDirectory() == true)
         {
             Debug.Log("Connect Success!");
         }
         else
         {
-            Debug.Log("Connect Failed!"); 
+            Debug.Log("Connect Failed!");
         }
     }
 
-    public bool ShowFtpFileAndDirectory()
+    /// <summary>
+    /// 展示FTP连接后的遍历结果
+    /// </summary>
+    /// <returns>是否连接上FTP服务器</returns>
+    private bool ShowFtpFileAndDirectory()
     {
-        if(currentDir == "/")
+        if (currentDir == "/")
         {
             GetBackButton.SetActive(false);
         }
@@ -97,7 +123,7 @@ public class GetFileFromFTP : MonoBehaviour {
             GetBackButton.SetActive(true);
         }
 
-        for(int i=0;i<UIPanel.childCount;i++)
+        for (int i = 0; i < UIPanel.childCount; i++)
         {
             Destroy(UIPanel.GetChild(i).gameObject);
         }
@@ -106,7 +132,7 @@ public class GetFileFromFTP : MonoBehaviour {
         try
         {
             string url = string.Empty;
-            if(currentDir == "/")
+            if (currentDir == "/")
             {
                 url = ftpUrlstring;
             }
@@ -119,7 +145,7 @@ public class GetFileFromFTP : MonoBehaviour {
 
             FtpWebResponse response = GetFtpResponse(request);
 
-            if(response == null)
+            if (response == null)
             {
                 Debug.Log("Response is null");
                 return false;
@@ -135,14 +161,16 @@ public class GetFileFromFTP : MonoBehaviour {
 
             //处理并显示文件目录列表
             string[] ftpDir = s.Split(Environment.NewLine.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
-            foreach(string tmp in ftpDir)
+            /*
+            foreach (string tmp in ftpDir)
             {
                 Debug.Log(tmp);
             }
+            */
             int length = 0;
-            for(int i = 0;i<ftpDir.Length;i++)
+            for (int i = 0; i < ftpDir.Length; i++)
             {
-                if(ftpDir[i].EndsWith("."))
+                if (ftpDir[i].EndsWith("."))
                 {
                     length = ftpDir[i].Length - 2;
                     break;
@@ -150,13 +178,13 @@ public class GetFileFromFTP : MonoBehaviour {
             }
 
 
-            for(int i = 0;i<ftpDir.Length;i++)
+            for (int i = 0; i < ftpDir.Length; i++)
             {
                 s = ftpDir[i];
                 int index = s.LastIndexOf('\t');
-                if(index == -1)
+                if (index == -1)
                 {
-                    if(length < s.Length)
+                    if (length < s.Length)
                     {
                         index = length;
                     }
@@ -167,12 +195,12 @@ public class GetFileFromFTP : MonoBehaviour {
                 }
 
                 string name = s.Substring(index + 1);
-                if(name == "."||name == "..")
+                if (name == "." || name == "..")
                 {
                     continue;
                 }
                 //判断是否为目录，在名称前加“Dirctory"来表示
-                if(s[0] == 'd' || (s.ToLower()).Contains("<dir>"))
+                if (s[0] == 'd' || (s.ToLower()).Contains("<dir>"))
                 {
                     string[] namefiled = name.Split(' ');
                     int namefiledlength = namefiled.Length;
@@ -180,7 +208,7 @@ public class GetFileFromFTP : MonoBehaviour {
                     dirname = namefiled[namefiledlength - 1];
                     dirname = dirname.PadRight(34, ' ');
                     name = dirname;
-                    Debug.Log("<Dir>:" + name);
+                    //Debug.Log("<Dir>:" + name);
                     GameObject tmp = GameObject.Instantiate(DirPrefab, UIPanel);
                     tmp.GetComponentInChildren<Text>().text = name.Replace(" ", ""); ;
                     tmp.GetComponent<Button>().onClick.AddListener(
@@ -200,13 +228,13 @@ public class GetFileFromFTP : MonoBehaviour {
 
             }
 
-            for(int i = 0;i < ftpDir.Length;i++)
+            for (int i = 0; i < ftpDir.Length; i++)
             {
                 s = ftpDir[i];
                 int index = s.LastIndexOf("\t");
-                if(index == -1)
+                if (index == -1)
                 {
-                    if(length < s.Length)
+                    if (length < s.Length)
                     {
                         index = length;
                     }
@@ -216,11 +244,11 @@ public class GetFileFromFTP : MonoBehaviour {
                     }
                 }
                 string name = s.Substring(index + 1);
-                if(name == "."||name == "..")
+                if (name == "." || name == "..")
                 {
                     continue;
                 }
-                if(!((s[0] == 'd')||(s.ToLower().Contains("<dir>"))))
+                if (!((s[0] == 'd') || (s.ToLower().Contains("<dir>"))))
                 {
                     string[] namefiled = name.Split(' ');
                     int namefiledlength = namefiled.Length;
@@ -228,13 +256,13 @@ public class GetFileFromFTP : MonoBehaviour {
                     filename = namefiled[namefiledlength - 1];
                     filename = filename.PadRight(34, ' ');
                     name = filename;
-                    Debug.Log("<File>:" + name);
+                    //Debug.Log("<File>:" + name);
 
                     GameObject tmp = GameObject.Instantiate(FilePrefab, UIPanel);
                     tmp.GetComponentInChildren<Text>().text = name.Replace(" ", "");
                     tmp.GetComponent<Button>().onClick.AddListener(
                         () => {
-                            DownloadFile(name);
+                            StartCoroutine(DownloadFile(name));
                         });
                     tmp.GetComponent<RectTransform>().anchoredPosition = AnchorPos;
 
@@ -242,21 +270,32 @@ public class GetFileFromFTP : MonoBehaviour {
             }
             return true;
         }
-        catch(Exception e)
+        catch (Exception e)
         {
             Debug.LogError(e.Message);
             return false;
         }
     }
 
+    /// <summary>
+    /// 点击返回上级按钮
+    /// </summary>
     public void ClickGetBackButton()
     {
+        FTPReturnToLastLevel();
+    }
+
+    /// <summary>
+    /// FTP返回上层级别
+    /// </summary>
+    public void FTPReturnToLastLevel()
+    {
         string t = "";
-        if(currentDir.EndsWith("/"))
+        if (currentDir.EndsWith("/"))
         {
             string[] tmp = currentDir.Split('/');
 
-            for(int i = 0;i<tmp.Length-2;i++)
+            for (int i = 0; i < tmp.Length - 2; i++)
             {
                 if (tmp[i] == " ")
                     continue;
@@ -287,10 +326,15 @@ public class GetFileFromFTP : MonoBehaviour {
         }
     }
 
-    public string GetUrlString(string filename)
+    /// <summary>
+    /// 得到目标文件路径
+    /// </summary>
+    /// <param name="filename">目标文件名</param>
+    /// <returns>目标文件的绝对文件路径</returns>
+    private string GetUrlString(string filename)
     {
         string url = string.Empty;
-        if(currentDir.EndsWith("/"))
+        if (currentDir.EndsWith("/"))
         {
             url = ftpUrlstring + currentDir + filename;
         }
@@ -301,35 +345,119 @@ public class GetFileFromFTP : MonoBehaviour {
         return url;
     }
 
-    public void DownloadFile(string filename)
+    /// <summary>
+    /// 下载文件
+    /// </summary>
+    /// <param name="filename">待下载的文件名</param>
+    public IEnumerator DownloadFile(string filename)
     {
+        slider.gameObject.SetActive(true);
+        slider.value = 0.0f;
+        slider.GetComponentInChildren<Text>().text = "0KB";
+        long downloadFileAllLength = 0;
+        int downloadFileAlreadyLength = 0;
         string savedFilePath = Application.dataPath + "/SaveFiles/" + filename;
+        string url = GetUrlString(filename);
         try
         {
-            string url = GetUrlString(filename);
-            FtpWebRequest request = CreatFtpWebRequest(url, WebRequestMethods.Ftp.DownloadFile);
-            FtpWebResponse response = GetFtpResponse(request);
-            if(response == null)
+            FtpWebRequest resFile = CreatFtpWebRequest(url, WebRequestMethods.Ftp.GetFileSize);
+            //resFile.UseBinary = true;
+            //resFile.UsePassive = true;
+            //resFile.Credentials = new NetworkCredential("nature", "Ttrby666");
+            using (FtpWebResponse res = (FtpWebResponse)resFile.GetResponse())
             {
-                return;
+                Debug.Log("All Length: " + res.ContentLength);
+                downloadFileAllLength = res.ContentLength;
+                slider.maxValue = res.ContentLength;
+                if (res == null)
+                {
+                    yield break;
+                }
             }
-            Stream responseStream = response.GetResponseStream();
-            FileStream fileStream = File.Create(savedFilePath);
+        }
+        catch (Exception e)
+        {
+            Debug.Log("Download filed: " + e.Message);
+        }
+
+        FtpWebRequest request = CreatFtpWebRequest(url, WebRequestMethods.Ftp.DownloadFile);
+        FtpWebResponse response = GetFtpResponse(request);
+        Stream responseStream = response.GetResponseStream();
+        FileStream fileStream = File.Create(savedFilePath);
+        int buflength = 8196;
+        byte[] buffer = new byte[buflength];
+        int bytesRead = 1;
+
+        while (bytesRead != 0)
+        {
+            bytesRead = responseStream.Read(buffer, 0, buflength);
+            fileStream.Write(buffer, 0, bytesRead);
+            downloadFileAlreadyLength += bytesRead;
+            Debug.Log("BytesRead:" + bytesRead + "AlreadyRead:" + downloadFileAlreadyLength);
+            yield return StartCoroutine(UpdateUISlider(downloadFileAlreadyLength));
+        }
+
+        Debug.Log("Download success!");
+        fileStream.Close();
+    }
+
+    public void OnclickUploadButton()
+    {
+        UploadFile("test.avi");
+    }
+
+    private void UploadFile(string newFileName)
+    {
+        try
+        {
+            string url = "ftp://127.0.0.1:14149/"+ newFileName;
+            string filepath = Application.dataPath + "/StreamingAssets/" + newFileName;
+
+            FtpWebRequest request = CreatFtpWebRequest(url, WebRequestMethods.Ftp.UploadFile);
+            Stream responseStream = request.GetRequestStream();
+
+            FileStream fs = File.OpenRead(filepath);
+            //FileStream fileStream = File.Create(url+"/testFile.txt");
             int buflength = 8196;
             byte[] buffer = new byte[buflength];
             int bytesRead = 1;
-            while(bytesRead != 0)
+
+            while (bytesRead != 0)
             {
-                bytesRead = responseStream.Read(buffer, 0, buflength);
-                fileStream.Write(buffer, 0, bytesRead);
+                bytesRead = fs.Read(buffer, 0, buflength);
+                responseStream.Write(buffer, 0, bytesRead);
             }
 
-            Debug.Log("Download success!");
-            fileStream.Close();
+            Debug.Log("Upload success!");
+            responseStream.Close();
         }
         catch(Exception e)
         {
-            Debug.Log("Download filed");
+            Debug.LogError(e.Message);
         }
+    }
+
+    private void OnFileUploadProgressChanged(System.Object sender, UploadProgressChangedEventArgs e)
+    {
+        Debug.Log("Uploading Progress: " + e.ProgressPercentage);
+    }
+
+    private void OnFileUploadCompleted(System.Object sender, UploadFileCompletedEventArgs e)
+    {
+        Debug.Log("File Uploaded Success!");
+    }
+
+    private IEnumerator UpdateUISlider(float value)
+    {
+        slider.value = value;
+        if (value == slider.maxValue)
+        {
+            slider.GetComponentInChildren<Text>().text = "Download Success!";
+        }
+        else
+        {
+            slider.GetComponentInChildren<Text>().text = ((long)value) + "KB/" + ((long)slider.maxValue) + "KB";
+        }
+        yield return new WaitForEndOfFrame();
     }
 }
---
+++ UnityProgram/FTPFileConnect/Assets/Scripts/UxlabFTP.cs
@@ -0,0 +1,354 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using UnityEngine.UI;
+
+namespace UxlabFTPServer
+{
+    public class UxlabFTP : MonoBehaviour
+    {
+        private static UxlabFTP instance;
+        public static UxlabFTP Instance
+        {
+            get
+            {
+                if(instance == null)
+                {
+                    instance = GameObject.FindObjectOfType<UxlabFTP>();
+                    if(instance == null)
+                    {
+                        GameObject go = new GameObject();
+                        instance = go.AddComponent<UxlabFTP>();
+                    }            
+                }
+                return instance;
+            }
+        }
+        
+        private string ftpUrlstring = null;             //FTP文件路径
+        private NetworkCredential networkCredential;    //网络证书
+        private string ftppath = "127.0.0.1:14149";     //FTP服务器地址
+        private string currentDir = "/";                //用于记录当前层级目录
+
+        private List<string> DirsList = new List<string>();         //获取FTP服务器中当前目录下的所有文件夹名称
+        private List<string> FilesList = new List<string>();        //获取FTP服务器中当前目录下所有的文件
+
+
+        #region 可调用的方法
+
+        /// <summary>
+        /// 连接FTP服务器，并在console界面展示连接返回结果
+        /// </summary>
+        public void ConnectToFTPServer(string userName,string password)
+        {
+            ftpUrlstring = "ftp://" + ftppath;
+
+            networkCredential = new NetworkCredential(userName, password);
+            
+            if (ShowFtpFileAndDirectory() == true)
+            {
+                Debug.Log("Connect Success!");
+            }
+            else
+            {
+                Debug.Log("Connect Failed!");
+            }
+        }
+
+        /// <summary>
+        /// FTP返回上层级别
+        /// </summary>
+        public void FTPReturnToLastLevel()
+        {
+            string t = "";
+            if (currentDir.EndsWith("/"))
+            {
+                string[] tmp = currentDir.Split('/');
+
+                for (int i = 0; i < tmp.Length - 2; i++)
+                {
+                    if (tmp[i] == " ")
+                        continue;
+                    t += tmp[i];
+                    t += "/";
+                }
+            }
+            else
+            {
+                string[] tmp = currentDir.Split('/');
+
+                for (int i = 0; i < tmp.Length - 1; i++)
+                {
+                    if (tmp[i] == " ")
+                        continue;
+                    t += tmp[i];
+                    t += "/";
+                }
+            }
+            currentDir = t;
+            if (ShowFtpFileAndDirectory() == true)
+            {
+                Debug.Log("Connect Success!");
+            }
+            else
+            {
+                Debug.Log("Connect Failed!");
+            }
+        }
+
+        /// <summary>
+        /// 下载指定文件到指定路径下
+        /// </summary>
+        /// <param name="filename">待下载的文件名，相对路径</param>
+        /// <param name="saveFileName">文件要存储的位置，绝对路径</param>
+        /// <returns></returns>
+        public IEnumerator DownloadFile(string filename, string saveFileName)
+        {
+            string savedFilePath = saveFileName;
+            string url = GetUrlString(filename);
+            try
+            {
+                FtpWebRequest resFile = CreatFtpWebRequest(url, WebRequestMethods.Ftp.GetFileSize);
+                //resFile.UseBinary = true;
+                //resFile.UsePassive = true;
+                //resFile.Credentials = new NetworkCredential("nature", "Ttrby666");
+                using (FtpWebResponse res = (FtpWebResponse)resFile.GetResponse())
+                {
+                    Debug.Log("All Length: " + res.ContentLength);
+                    if (res == null)
+                    {
+                        yield break;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.Log("Download filed: " + e.Message);
+            }
+
+            FtpWebRequest request = CreatFtpWebRequest(url, WebRequestMethods.Ftp.DownloadFile);
+            FtpWebResponse response = GetFtpResponse(request);
+            Stream responseStream = response.GetResponseStream();
+            FileStream fileStream = File.Create(savedFilePath);
+            int buflength = 8196;
+            byte[] buffer = new byte[buflength];
+            int bytesRead = 1;
+
+            while (bytesRead != 0)
+            {
+                bytesRead = responseStream.Read(buffer, 0, buflength);
+                fileStream.Write(buffer, 0, bytesRead);
+                //Debug.Log("BytesRead:" + bytesRead + "AlreadyRead:" + downloadFileAlreadyLength);
+                yield return new WaitForEndOfFrame();
+            }
+
+            Debug.Log("Download success!");
+            fileStream.Close();
+        }
+
+        /// <summary>
+        /// 获取当前FTP文件目录下文件夹
+        /// </summary>
+        /// <returns>返回FTP当前文件目录下的文件夹名称</returns>
+        public List<string> GetDirectories()
+        {
+            return DirsList;
+        }
+
+        /// <summary>
+        /// 获取当前FTP文件目录下的所有文件名
+        /// </summary>
+        /// <returns>返回FTP当前文件下的文件名称</returns>
+        public List<string> GetFiles()
+        {
+            return FilesList;
+        }
+
+        #endregion
+
+
+        #region 私有函数
+        /// <summary>
+        /// 创建FTP服务器连接
+        /// </summary>
+        /// <param name="url">FTP服务器地址</param>
+        /// <param name="requestMethod">FTP服务器请求方法</param>
+        /// <returns></returns>
+        private FtpWebRequest CreatFtpWebRequest(string url, string requestMethod)
+        {
+            FtpWebRequest request = (FtpWebRequest)FtpWebRequest.Create(url);
+            request.Credentials = networkCredential;
+            request.KeepAlive = true;
+            request.UseBinary = true;
+            request.Method = requestMethod;
+            return request;
+        }
+
+        /// <summary>
+        /// 获取Ftp服务器返回的响应体
+        /// </summary>
+        /// <param name="request">发出的对FTP服务器请求</param>
+        /// <returns></returns>
+        private FtpWebResponse GetFtpResponse(FtpWebRequest request)
+        {
+            FtpWebResponse response = null;
+            try
+            {
+                response = (FtpWebResponse)request.GetResponse();
+                return response;
+            }
+            catch (WebException ex)
+            {
+                Debug.LogError(ex.Message);
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// 展示FTP连接后的遍历结果
+        /// </summary>
+        /// <returns>是否连接上FTP服务器</returns>
+        private bool ShowFtpFileAndDirectory()
+        {
+            try
+            {
+                DirsList.Clear();
+                FilesList.Clear();
+                string url = string.Empty;
+                if (currentDir == "/")
+                {
+                    url = ftpUrlstring;
+                }
+                else
+                {
+                    url = ftpUrlstring + currentDir;
+                }
+                Debug.Log(url);
+                FtpWebRequest request = CreatFtpWebRequest(url, WebRequestMethods.Ftp.ListDirectoryDetails);
+                FtpWebResponse response = GetFtpResponse(request);
+                if (response == null)
+                {
+                    Debug.Log("Response is null");
+                    return false;
+                }
+
+                //读取网络流数据
+                Stream stream = response.GetResponseStream();
+                StreamReader streamReader = new StreamReader(stream, Encoding.Default);
+                string s = streamReader.ReadToEnd();
+                streamReader.Close();
+                response.Close();
+
+                //处理并显示文件目录列表
+                string[] ftpDir = s.Split(Environment.NewLine.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+                int length = 0;
+                for (int i = 0; i < ftpDir.Length; i++)
+                {
+                    if (ftpDir[i].EndsWith("."))
+                    {
+                        length = ftpDir[i].Length - 2;
+                        break;
+                    }
+                }
+                for (int i = 0; i < ftpDir.Length; i++)
+                {
+                    s = ftpDir[i];
+                    int index = s.LastIndexOf('\t');
+                    if (index == -1)
+                    {
+                        if (length < s.Length)
+                        {
+                            index = length;
+                        }
+                        else
+                        {
+                            continue;
+                        }
+                    }
+
+                    string name = s.Substring(index + 1);
+                    if (name == "." || name == "..")
+                    {
+                        continue;
+                    }
+                    //判断是否为目录，在名称前加“Dirctory"来表示
+                    if (s[0] == 'd' || (s.ToLower()).Contains("<dir>"))
+                    {
+                        string[] namefiled = name.Split(' ');
+                        int namefiledlength = namefiled.Length;
+                        string dirname;
+                        dirname = namefiled[namefiledlength - 1];
+                        dirname = dirname.PadRight(34, ' ');
+                        name = dirname;
+                        Debug.Log("<Dir>:" + name);
+                        DirsList.Add(name.Replace(" ", ""));
+                    }
+
+                }
+                for (int i = 0; i < ftpDir.Length; i++)
+                {
+                    s = ftpDir[i];
+                    int index = s.LastIndexOf("\t");
+                    if (index == -1)
+                    {
+                        if (length < s.Length)
+                        {
+                            index = length;
+                        }
+                        else
+                        {
+                            continue;
+                        }
+                    }
+                    string name = s.Substring(index + 1);
+                    if (name == "." || name == "..")
+                    {
+                        continue;
+                    }
+                    if (!((s[0] == 'd') || (s.ToLower().Contains("<dir>"))))
+                    {
+                        string[] namefiled = name.Split(' ');
+                        int namefiledlength = namefiled.Length;
+                        string filename;
+                        filename = namefiled[namefiledlength - 1];
+                        filename = filename.PadRight(34, ' ');
+                        name = filename;
+                        Debug.Log("<File>:" + name);
+                        FilesList.Add(name.Replace(" ", ""));
+                    }
+                }
+                return true;
+            }
+            catch (Exception e)
+            {
+                Debug.LogError(e.Message);
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// 得到目标文件路径
+        /// </summary>
+        /// <param name="filename">目标文件名</param>
+        /// <returns>目标文件的绝对文件路径</returns>
+        private string GetUrlString(string filename)
+        {
+            string url = string.Empty;
+            if (currentDir.EndsWith("/"))
+            {
+                url = ftpUrlstring + currentDir + filename;
+            }
+            else
+            {
+                url = ftpUrlstring + currentDir + "/" + filename;
+            }
+            return url;
+        }
+        #endregion
+    }
+}
+
